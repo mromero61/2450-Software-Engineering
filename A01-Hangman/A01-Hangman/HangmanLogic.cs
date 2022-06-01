fa3c5586c477072005584b5f12617cfda9feaa22
Marcell Romero <mromer61@bruinmail.slcc.edu>
2022-06-01 08:57:25 -0600

    Added HangmanLogic class and logic
    Added the class which gives access to the variables and also did the beginning logics.

---
+++ A01-Hangman/A01-Hangman/HangmanLogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A01_Hangman
+{
+    internal class HangmanLogic
+    {
+        private int lives;
+        private string secretWord;
+
+        public HangmanLogic()
+        {
+            Lives = 6;
+            SecretWord = "Utah";
+        }
+
+        public int Lives { get => lives; set => lives = value; }
+        public string SecretWord { get => secretWord; set => secretWord = value; }
+
+        public void Greetings()
+        {
+            Console.WriteLine("Welcome to Hangman! Type in your guesses to figure out the secret word! But be careful you only have 6 lives!\n");
+        }
+
+        
+
+    }
+}
--- A01-Hangman/A01-Hangman/Program.cs
+++ A01-Hangman/A01-Hangman/Program.cs
@@ -8,8 +8,26 @@ namespace A01_Hangman
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            bool wordGuessed = true;
+            string letterGuessed;
+            string secretWord;
+            HangmanLogic hangmanLogic = new HangmanLogic();
+
+            secretWord = hangmanLogic.SecretWord;
+            hangmanLogic.Greetings();
+            Console.WriteLine(String.Join("", secretWord.Select(v => "_")) + " lives left: {0} \n", hangmanLogic.Lives);
+
+            while (wordGuessed)
+            {
+                Console.WriteLine("What is your guess: ");
+                letterGuessed = Console.ReadLine();
+                
+            }
+
         }
+
     }
 }
